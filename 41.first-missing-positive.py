24972d3d424211bff9780624cecae285a9030846
Owen Xu <owenxu10@aliyun.com>
2017-08-20 17:21:14 -0400

    41 need to redo, not the best O(n)

---
+++ 41.first-missing-positive.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 1
+        for i in xrange(1, len(nums)+2):
+            if i not in nums:
+                return i
+        
+
